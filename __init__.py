5caf34786a2383beed0832fd3e22a40f50758ccc
Hassan Raya <hassanraya76@gmail.com>
2022-09-12 10:55:31 +0200

    addOne, removeOne, deleteAll

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
+from http.client import NOT_FOUND, OK
+import re
 import sqlite3
 import init_db
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, Response, request
 
 app = Flask(__name__)
 
@@ -13,12 +15,60 @@ def get_db_connection():
         print(e)
     return conn
 
-@app.route('/')
-def index():
+@app.route('/addOne', methods=['POST'])
+def add_one():
+    product_id = request.args.get('id')
+    conn = get_db_connection()
+    res = conn.execute("""Update things set quantity=quantity+1 where id=?""", product_id)
+    conn.commit()
+    conn.close()
+    if (res.rowcount == 1):
+        return Response(status=OK)
+    else:
+        return Response(status=NOT_FOUND)
+
+
+@app.route('/removeOne', methods=['POST'])
+def remove_one():
+    product_id = request.args.get('id')
+    conn = get_db_connection()
+    actual_quantity = conn.execute("""Select quantity from things where id=?""", product_id).fetchall()
+    actual_quantity = actual_quantity[0]['quantity']
+    if (actual_quantity >= 1):
+        res = conn.execute("""Update things set quantity=quantity-1 where id=?""", product_id)
+        conn.commit()
+    conn.close()
+    if (res.rowcount == 1):
+        return Response(status=OK)
+    else:
+        return Response(status=NOT_FOUND)
+
+
+@app.route('/deleteAll', methods=['POST'])
+def delete_all():
+    product_id = request.args.get('id')
+    conn = get_db_connection()
+    res = conn.execute("""Delete from things where id=?""", product_id)
+    conn.commit()
+    conn.close()
+    if (res.rowcount == 1):
+        return Response(status=OK)
+    else:
+        return Response(status=NOT_FOUND)
+
+
+
+@app.route('/getAll')
+def get_all():
     conn = get_db_connection()
     things = conn.execute('SELECT * FROM things').fetchall()
+    things = [tuple(row) for row in things]
     conn.close()
-    return render_template('index.html', things=things)
+    return jsonify(things)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     init_db
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -5,17 +5,61 @@ const { createApp, ref } = Vue
       return {
         drawer: ref(false),
         slide: ref(1),
-        searchText: ref('')
+        searchText: ref(''),
+        productList: ref([])
       }
     },
 
     methods: {
       
+      getAll() {
+        axios.get("/getAll")
+        .then(response => {
+          this.productList = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        })
+      },
+
+      addOne(id) {
+        axios.post("/addOne?id=" + id)
+        .then(response => {
+          this.getAll();
+        })
+        .catch(error => {
+          console.log(error);
+        })
+      },
+
+      removeOne(id) {
+        axios.post("/removeOne?id=" + id)
+        .then(response => {
+          this.getAll();
+        })
+        .catch(error => {
+          console.log(error);
+        })
+      },
+
+      deleteAll(id) {
+        axios.post("/deleteAll?id=" + id)
+        .then(response => {
+          this.getAll();
+        })
+        .catch(error => {
+          console.log(error);
+        })
+      },
+
+      modify(id) {
+
+      }
 
     },
 
     async created() {
-      
+      await this.getAll();
     },
 
     delimiters: ['{', '}']

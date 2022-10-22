46503ca566bbfe19fca868af61540e50daa69fa2
Hassan Raya <hassanraya76@gmail.com>
2022-10-22 19:11:23 +0200

    Cypress test

--- cypress/e2e/grocery.cy.js
+++ cypress/e2e/grocery.cy.js
@@ -5,8 +5,8 @@ describe('grocery list', () => {
 
   it('Add product', () => {
     cy.contains("Add Product").click()
-    cy.contains("Product name *").find('input').type('Cypress Test')
-    cy.contains("Product quantity *").find('input').type(5)
+    cy.contains("Product name *").find('input').clear().type('Cypress Test')
+    cy.contains("Product quantity *").find('input').clear().type(5)
     cy.contains("Submit").click()
   })
 
@@ -41,7 +41,7 @@ describe('grocery list', () => {
 
   it('Modify product', () => {
     cy.contains("Cypress Test").parent().parent().find('i:contains("edit")').click()
-    cy.contains("Product name *").find('input').type('Cypress Test Modif')
+    cy.contains("Product name *").find('input').clear().type('Cypress Test Modif')
     cy.contains("Product quantity *").find('input').clear().type(10)
     cy.contains("Submit").click()
   })
@@ -52,6 +52,37 @@ describe('grocery list', () => {
       "10"
     );
   })
+
+  it('Search product', () => {
+    cy.contains("Search").parent().find('input').clear().type('T')
+    cy.contains("Cypress Test Modif").parent().find(".text-subtitle1").should(
+      "have.text",
+      "10"
+    )
+    cy.contains("Search").parent().find('input').clear().type('TM')
+    cy.get('.q-page').find(".q-list").should('have.length', 0)
+  })
+
+  it('Delete Product', () => {
+    cy.contains("Search").parent().find('input').clear()
+    cy.contains("Cypress Test").parent().parent().find('i:contains("delete")').click()
+    cy.get('.q-page').find(".q-list").should('have.length', 0)
+  })
+  
+  it('Menu', () => {
+    cy.get(".q-layout--prevent-focus").should('have.length', 0)
+    cy.contains("menu").click()
+  })
+
+  it ('Hide Drawer', () => {
+    cy.get(".q-layout--prevent-focus").should('have.length', 1)
+  })
+
+  it ('Store click', () => {
+    cy.contains("menu").click()
+    cy.contains('store').parent().parent().click()
+  })
+
 
 
 
